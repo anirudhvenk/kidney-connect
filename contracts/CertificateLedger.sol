1816c1c5c7a2be6e9e76e12f9c16734abf0154df
anirudhvenkatraman <anirudhv05ca@gmail.com>
2021-04-16 21:27:09 -0700

    Finished basic UI and basic backend

--- contracts/CertificateLedger.sol
+++ contracts/CertificateLedger.sol
@@ -1,39 +1,70 @@
 pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
 
 contract CertificateLedger {
     uint256 certCount = 0;
     struct Certificate {
-        string CertificateID;
-        // uint256 RecipientID;
+        string recipientID;
         // uint256 RecipientHealth;
         // uint256 DonorID;
         // uint256 DonorHealth;
-        // address DoctorSig;
+        address signature;
+        Health recipientHealth;
+
         // uint256 Contact;
         // uint256 TimeStamp;
         // bool ValidPair;
     }
 
+    struct Health {
+        uint256 age;
+    }
+
     mapping(uint256 => Certificate) public certificates;
 
-    // event certificateCreated(Certificate);
-    event matchFound(string certificateID);
+    constructor() public {}
 
-    function createCertificate(string memory _content) public {
-        certificates[certCount] = Certificate(_content);
-        // emit certificateCreated(certificates[certCount]);
+    function getAddress(uint256 id) public view returns (uint256) {
+        return (10);
+    }
+
+    function getRecipientHealth(uint256 id) public view returns (uint256) {
+        return (certificates[id].recipientHealth.age);
+    }
+
+    function createCert(
+        address _signature,
+        string memory _recipientID,
+        Health memory _recipientHealth
+    ) public {
+        certificates[certCount] = Certificate(
+            _recipientID,
+            _signature,
+            _recipientHealth
+        );
         certCount++;
     }
 
-    function findMatch(string memory _certificateID)
-        public
-        returns (mapping(uint256 => Certificate))
-    {
-        // for (uint256 j = 0; j < certCount; j++) {
-        // }
-        // emit matchFound(_certificateID);
-        // uint256[] x = [1, 2, 3];
-        string[] ids = ["a", "b", "c"];
-        return (ids);
-    }
+    // mapping(uint256 => Certificate) public certificates;
+
+    // // event certificateCreated(Certificate);
+    // event matchFound(string certificateID);
+
+    // function createCertificate(string memory _content) public {
+    //     CertificateLedger cert = Certificate(_content);
+    //     // emit certificateCreated(certificates[certCount]);
+    //     certCount++;
+    // }
+
+    // function findMatch(address addr) external returns (uint256) {
+    //     // for (uint256 j = 0; j < certCount; j++) {
+    //     // }
+    //     // emit matchFound(_certificateID);
+    //     // uint256[] x = [1, 2, 3];
+    //     return (10);
+    // }
 }
+
+// contract CertificateLedgerWrapper {
+//     uint256 certificates = 10;
+// }
